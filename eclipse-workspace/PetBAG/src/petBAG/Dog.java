83a1d58969282fa87d54b3fb813e677c87514186
Jeff <jmperkinson@gmail.com>
2019-09-06 16:26:09 -0400

    Add setter methods to Dog class.

--- eclipse-workspace/PetBAG/src/petBAG/Dog.java
+++ eclipse-workspace/PetBAG/src/petBAG/Dog.java
@@ -11,6 +11,9 @@ public class Dog {
     private int catSpace;
     private int daysStay;
     private double amountDue;
+    public int dogSpaceNbr;
+    public int dogWeight;
+    public boolean grooming;
     
     /*Class constructor.
     * With the checkIn() method, petType determines the object type to be 
@@ -35,6 +38,72 @@ public class Dog {
         this.catSpace = catSpace;
         daysStay = -1;
         amountDue = -1.0;
+        dogSpaceNbr = -1;
+        dogWeight = -1;
+        grooming = false;
     }
+    
+    /*Setters and Getters
+     * Setter methods establish an interface to write class fields.
+     * The concept of encapsulation is implemented by way of making read and
+     * write access to private fields available to other classes through
+     * public methods.  Setters provide write functionality to update the field.
+     * Getters provide read access to provide visibility into the field value.*/
+    
+    //Setters.  Void return type because they don't return a value.
+    
+    public void setPetType(String petType) {
+        this.petType = petType;
+        return;
+    }
+    
+    public void setPetName(String petName) {
+        this.petName = petName;
+        return;
+    }
+    
+    public void setPetAge(int petAge) {
+        this.petAge = petAge;
+        return;
+    }
+    
+    public void setDogSpace(int dogSpace) {
+        this.dogSpace = dogSpace;
+        return;
+    }
+    
+    public void setCatSpace(int catSpace) {
+        this.catSpace = catSpace;
+        return;
+    }
+    
+    public void setDaysStay(int daysStay) {
+        this.daysStay = daysStay;
+        return;
+    }
+    
+    public void setAmountDue (double amountDue) {
+        this.amountDue = amountDue;
+        return;
+    }
+    
+    public void setDogSpaceNbr(int dogSpaceNbr) {
+        this.dogSpaceNbr = dogSpaceNbr;
+        return;
+    }
+    
+    public void setDogWeight(int dogWeight) {
+        this.dogWeight = dogWeight;
+        return;
+    }
+    
+    public void setGroomming(boolean grooming) {
+        this.grooming = grooming;
+        return;
+    }
+    
+    //Getters.  Return type is the data type of the field being returned.
+    
+    
     
 }
