5e175c136d98692f55697883d99e13138bd11fb7
Jeff <jmperkinson@gmail.com>
2019-09-11 12:22:39 -0400

    Add Pet class variables, setters/getters, tests.
    This commit adds the setters and getters for the Pet class.
    The dogSpace and catSpace fields are also converted
    to class variables by way of 'static'.  PetBAG tests are
    added to test the default constructor and modification
    of class variables through an object instance.

--- eclipse-workspace/PetBAG/src/petBAG/Pet.java
+++ eclipse-workspace/PetBAG/src/petBAG/Pet.java
@@ -11,9 +11,18 @@ public class Pet {
     private String petType;
     private String petName;
     private int petAge;
-    //TODO: Address dogSpace/catSpace initialization and comment about it.
-    private int dogSpace;
-    private int catSpace;
+    /* The the checkIn() method decrements the dogSpace and catSpace
+     * fields when a pet is checked in.  Due to scope constraints, these
+     * field values must be defined outside any class method so they 
+     * are accessible throughout the program.  They should not be set as a 
+     * predefined value.  We want to have these fields common to all objects. 
+     * This is accomplished with the static modifier to make these class variables.
+     * are associated with the class- each instance of the class shares the class variable.
+     * Any object can change the value of a class variable.
+     * https://docs.oracle.com/javase/tutorial/java/javaOO/classvars.html
+     */
+    private static int dogSpace = 30;
+    private static int catSpace = 12;
     private int daysStay;
     private double amountDue;
     
@@ -46,20 +55,102 @@ public class Pet {
         petAge = -1;
        /* When a Pet is checked in under the checkIn() method, the dogSpace
         * and catSpace fields are decremented to record the space reduction.  
-        * Subsequent instantiations should look to the global dogSpace and catSpace
+        * Subsequent instantiations should look to the class variable dogSpace and catSpace
         * values to reflect any updates to space availability from prior
         * calls to checkIn() and can therefore not be a hard-coded value.
-        */
-        // TODO: fix dogSpace and catSpace instantiation?
-        dogSpace = dogSpace;
-        catSpace = catSpace;
+        * That these value changes will be modified by each object checkIn()
+        * is inconsequential because prior to decrementing the value will be assigned to
+        * the Cat or Dog object's catSpaceNbr and dogSpaceNbr, respectively. 
+        */ 
+        this.dogSpace = dogSpace;
+        this.catSpace = catSpace;
         daysStay = -1;
         amountDue = -1.0;
     }
     
-    // Constructor with petType, petName, dogSpace, catSpace parameters
-    public Pet(String petType, String petName, int dogSpace, int catSpace) {
-        
+    // Constructor with petType and petName parameters
+//    public Pet(String petType, String petName) {
+//        
+//    }
+    
+    /* Setters and Getters
+     * Setter methods establish an interface to write class fields.
+     * The concept of encapsulation is implemented by way of making read and
+     * write access to private fields available to other classes through
+     * public setter and getter methods.  Setters provide write functionality to update 
+     * the field. Getters provide read access and provide visibility into the field value.
+     */
+    
+    /* Setters. Void return type because they don't return a value.
+     * The methods still have a return statement even though return type is void
+     * in order to clear the stack frame (i.e. local variables created for the method).
+     */
+    
+    public void setPetType(String petType) {
+        this.petType = petType;
+        return;
     }
     
+    public void setPetName(String petName) {
+        this.petName = petName;
+        return;
+    }
+    
+    public void setPetAge(int petAge) {
+        this.petAge = petAge;
+        return;
+    }
+    
+    public void setDogSpace(int dogSpace) {
+        this.dogSpace = dogSpace;
+        return;
+    }
+    
+    public void setCatSpace(int catSpace) {
+        this.catSpace = catSpace;
+        return;
+    }
+    
+    public void setDaysStay(int daysStay) {
+        this.daysStay = daysStay;
+        return;
+    }
+    
+    public void setAmountDue (double amountDue) {
+        this.amountDue = amountDue;
+        return;
+    }
+    
+    /* Getters. Return type is the data type of the field being returned.
+     * Getter methods take no arguments when called and therefore
+     * do not need to be defined with any parameters in the method signature.
+     */
+    
+    public String getPetType() {
+        return petType;
+    }
+    
+    public String getPetName() {
+        return petName;
+    }
+    
+    public int getPetAge() {
+        return petAge;
+    }
+    
+    public int getDogSpace() {
+        return dogSpace;
+    }
+    
+    public int getCatSpace() {
+        return catSpace;
+    }
+    
+    public int getDaysStay() {
+        return daysStay;
+    }
+    
+    public double getAmountDue() {
+        return amountDue;
+    }
 }
--- eclipse-workspace/PetBAG/src/petBAG/petBAG.java
+++ eclipse-workspace/PetBAG/src/petBAG/petBAG.java
@@ -4,6 +4,32 @@ public class petBAG {
 
     public static void main(String[] args) {
         // Main program to test the Pet class
+        
+        Pet cat1 = new Pet();
+        
+        // Test default constructor
+        System.out.println("*****DEFAULT CONSTRUCTOR******");
+        System.out.println("Pet Type: " + cat1.getPetType());
+        System.out.println("Pet Name: " + cat1.getPetName());
+        System.out.println("Pet Age: " + cat1.getPetAge());
+        System.out.println("Dog Space: " + cat1.getDogSpace());
+        System.out.println("Cat Space: " + cat1.getCatSpace());
+        System.out.println("Days Stay: " + cat1.getDaysStay());
+        System.out.println("Amount Due: " + cat1.getAmountDue());
+        
+        // Test class variable changes and default constructor
+        cat1.setDogSpace(12);
+        cat1.setCatSpace(11);
+        
+        Pet cat2 = new Pet();
+        System.out.println("*****DEFAULT CONSTRUCTOR******");
+        System.out.println("Pet Type: " + cat2.getPetType());
+        System.out.println("Pet Name: " + cat2.getPetName());
+        System.out.println("Pet Age: " + cat2.getPetAge());
+        System.out.println("Dog Space: " + cat2.getDogSpace());
+        System.out.println("Cat Space: " + cat2.getCatSpace());
+        System.out.println("Days Stay: " + cat2.getDaysStay());
+        System.out.println("Amount Due: " + cat2.getAmountDue());
 
     }
 
