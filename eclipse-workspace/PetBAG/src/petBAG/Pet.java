b021d8b84878c9963516eac050a5dcaf072f1e27
Jeff <jmperkinson@gmail.com>
2019-09-11 12:52:52 -0400

    Adds parameterized Pet constructor.
    This commit adds the parameterized constructor for
    the Pet class.  It utilizes values for petType
    and petName to be passed in.

--- eclipse-workspace/PetBAG/src/petBAG/Pet.java
+++ eclipse-workspace/PetBAG/src/petBAG/Pet.java
@@ -31,15 +31,18 @@ public class Pet {
     /* Class constructors.
      * With the checkIn() method defined later, petType input determines the 
      * object type to be instantiated (i.e. Cat or Dog).  
-     * Passing petType into the constructor saves having to call setPetType separately.
+     * Passing petType into a parameterized constructor saves having to call setPetType separately.
      * The checkIn() method also calls for the user to determine whether
      * the client is a new or returning customer.  Passing petName 
-     * into the class constructor saves having to call setPetName
+     * into a parameterized constructor saves having to call setPetName
      * separately and can also aid in any type of future enhancement record search 
      * that might entail cross-referencing against database records.
+     * The Dog and Cat classes will need to include a parameterized constructor
+     * that calls the base class's parameterized constructor.  This parameterized constructor
+     * should be called for each respective Dog and Cat object instantiated during checkIn().
      * If any constructor is defined, the compiler does not implicitly define a 
      * default constructor (i.e. no parameters).  Best practice is to explicitly
-     * define a default constructor and rely on method overloading for other constructors
+     * define a default constructor and rely on method overloading for parameterized constructors
      * so that object creation like Pet pet = new Pet() without arguments remains
      * supported.  The compiler will throw an error if the default constructor is not defined
      * and an attempt to instantiate an object without arguments is made.
@@ -55,25 +58,43 @@ public class Pet {
          * fields are initialized to -1 for their respective data types.   
          */
         petAge = -1;
-       /* When a Pet is checked in under the checkIn() method, the dogSpace
-        * and catSpace fields are decremented to record the space reduction.  
-        * Subsequent instantiations should look to the class variable dogSpace and catSpace
-        * values to reflect any updates to space availability from prior
-        * calls to checkIn() and can therefore not be a hard-coded value.
-        * That these value changes will be modified by each object checkIn()
-        * is inconsequential because prior to decrementing the value will be assigned to
-        * the Cat or Dog object's catSpaceNbr and dogSpaceNbr, respectively. 
-        */ 
+        /* When a Pet is checked in under the checkIn() method, the dogSpace
+         * and catSpace fields are decremented to record the space reduction.  
+         * Subsequent instantiations should look to the class variable dogSpace and catSpace
+         * values to reflect any updates to space availability from prior
+         * calls to checkIn() and can therefore not be a hard-coded value.
+         * That these value changes will be modified by each object checkIn()
+         * is inconsequential because prior to decrementing the value will be assigned to
+         * the Cat or Dog object's catSpaceNbr and dogSpaceNbr, respectively. 
+         * 
+         * While these dogSpace and catSpace initialization statements are redundant,
+         * they are added for program readability to show that the object's dogSpace and catSpace
+         * field values are being initialized to the class variable values.
+         */
         this.dogSpace = dogSpace;
         this.catSpace = catSpace;
         daysStay = -1;
         amountDue = -1.0;
     }
     
-    // Constructor with petType and petName parameters
-//    public Pet(String petType, String petName) {
-//        
-//    }
+    // Parameterized constructor with petType and petName parameters
+    public Pet(String petType, String petName) {
+        this.petType = petType;
+        this.petName = petName;
+        /* Just as above, fields of type int and double are initialized to a
+         * default value of -1/-1.0 when the constructor method signature 
+         * does not include a parameter for that field.   
+         */
+        petAge = -1;
+        /* While these dogSpace and catSpace initialization statements are redundant,
+         * they are added for program readability to show that the object's dogSpace and catSpace
+         * field values are being initialized to the class variable values.
+         */
+        this.dogSpace= dogSpace;
+        this.catSpace= catSpace;
+        daysStay = -1;
+        amountDue = -1.0;
+    }
     
     /* Setters and Getters
      * Setter methods establish an interface to write class fields.
