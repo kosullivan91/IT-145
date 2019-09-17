425a15c7331c5831a5a9d7c8e6f98160c08a3776
Jeff <jmperkinson@gmail.com>
2019-09-17 06:30:04 -0400

    Initial commit of DogApp.

---
+++ eclipse-workspace/DogApp/Dog.java
@@ -0,0 +1,20 @@
+public class Dog {
+
+    // class variables
+
+
+    // constructor
+
+
+    // methods
+
+
+    // method used to print Dog information
+    public String toString() {
+        String temp = "\nDOG DATA\n" + name + " is a " + breed +
+                ", a " + type + " dog. \nThe top trick is : " +
+                topTrick + ".";
+        return temp;
+    }
+
+}
