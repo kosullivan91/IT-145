6bb419bfd7f4d2dc6cb8e3479be995078bfe855f
Jeff <jmperkinson@gmail.com>
2019-09-21 05:24:38 -0400

    Add final version of lab 4 PeopleWeights.java.

--- eclipse-workspace/IT145ClassLabs/src/PeopleWeights.java
+++ eclipse-workspace/IT145ClassLabs/src/PeopleWeights.java
@@ -1,3 +1,4 @@
+// import Java's Scanner class to retrieve user input.
 import java.util.Scanner;
 
 public class PeopleWeights {
@@ -6,40 +7,41 @@ public class PeopleWeights {
         
         /* Program variables:
          * Create a Scanner object to capture user input
-         * Declare a final int variable to serve as the array length; declaring
+         * Declare a final int variable to serve as the array length.  Declaring
          * array length based on an int variable makes array size modification
-         * simple through changing one line of code only.
+         * simple through only needing to change one line of code.
          * Declare a totalWeight double to capture the sum of all input and 
          * initialize it to 0.0.
          * Declare an averageWeight double to capture the average of all input and
          * initialize it to 0.0. 
-         * Declare the maxWeight double to capture the max value input and
+         * Declare the maxWeight double to capture the max input value and
          * initialize it to 0.0. 
          */
         
         Scanner scnr = new Scanner(System.in);
-        final int NUM_OF_ELEMENTS = 5; // The assignment calls for an array of length 5.
-        double[] peopleWeight = new double[NUM_OF_ELEMENTS];
+        final int NUM_OF_ELEMENTS = 5; 
+        // The array assignment calls for an array of type double with length NUM_OF_ELEMENTS.
+        double[] peopleWeight = new double[NUM_OF_ELEMENTS]; 
         double totalWeight = 0.0;
         double averageWeight = 0.0;
         double maxWeight = 0.0;
         
         /* Prompt the user to enter five numbers.  Store the input in an array of doubles.
          * Doubles array peopleWeight was declared above.  The user is prompted for input
-         * five times, so the number of iterations is known (i.e. array length).  A for loop 
-         * should therefore be used in lieu of a while loop.
+         * five times, so the number of iterations is known (i.e. the array length).  
+         * A for loop should therefore be used in lieu of a while loop.
          * Iterate from 0 up to the array length (exclusive) to account for all element
          * indices (as indices start at 0 and continue to one less than the array length).  
          */
                 
         for (int i = 0; i < peopleWeight.length; ++i) {
             System.out.println("Enter weight " + (i + 1) + ": ");
-            //Assign the user input to the array element at each iteration's index.
+            // Assign the user input to the array element at each iteration's index.
             peopleWeight[i] = scnr.nextDouble();
-            //Add the element value at each iteration's index to totalWeight.
+            // Add the element value at each iteration's index to totalWeight.
             totalWeight += peopleWeight[i];
         }
-        System.out.println("");
+        System.out.println();
         
         /* Output the array's numbers on one line, each number followed by one space.
          * Just as above, a for loop is used because the number of iterations is known.
@@ -51,14 +53,15 @@ public class PeopleWeights {
         for (int i = 0; i < peopleWeight.length; ++i) {
             System.out.print(peopleWeight[i] + " ");
         }
-        System.out.println("");
+        System.out.println();
         
-        //Output the total weight (i.e. the sum of all array elements).
+        // Output the total weight (i.e. the sum of all array elements).
         System.out.println("Total weight: " + totalWeight);
         
         /* Output the average weight.
-         * The divisor should be the length of the array to ensure it's reflective of the
-         * true average should the array's size change.
+         * The divisor should be the length of the array to ensure it's reflective
+         * of the true average if the array's size is changed above
+         * by modifying the value assigned to NUM_OF_ELEMENTS.
          */
         averageWeight = totalWeight / peopleWeight.length;
         System.out.println("Average weight: " + averageWeight);
