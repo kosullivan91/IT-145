5458e8675d65dfe06d530b8482ec651987e7eb91
Jeff <jmperkinson@gmail.com>
2019-09-23 20:23:37 -0400

    Define Driver class for DogApp.

--- eclipse-workspace/DogApp/Driver.java
+++ eclipse-workspace/DogApp/Driver.java
@@ -2,7 +2,15 @@
 public class Driver {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        // instantiate the corgi object
+        Corgi corgi = new Corgi ("cattle herding", "Pembroke Welsh Corgi", "Wales", 31, 12);
+        
+        // set top trick for corgi object
+        corgi.setToptrick("Lassoing cattle");
+        
+        // print the output to the console
+        System.out.println(corgi.toString());
 
     }
 
