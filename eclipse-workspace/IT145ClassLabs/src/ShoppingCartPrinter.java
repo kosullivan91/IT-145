d1d6202927670304654cc159542b3b6f642dc810
Jeff <jmperkinson@gmail.com>
2019-09-24 06:01:25 -0400

    Add user prompts to ShoppingcartPrinter class.

--- eclipse-workspace/IT145ClassLabs/src/ShoppingCartPrinter.java
+++ eclipse-workspace/IT145ClassLabs/src/ShoppingCartPrinter.java
@@ -2,21 +2,53 @@ import java.util.Scanner;
 
 public class ShoppingCartPrinter {
    public static void main(String[] args) {
+      //create a Scanner object to collect user input.
       Scanner scnr = new Scanner(System.in);
+      
+      //field declaration and initialization.
       int i = 0;
       String productName;
       int productPrice = 0;
       int productQuantity = 0;
       int cartTotal = 0;
   
+      //item object default initialization using default constructor.
       ItemToPurchase item1 = new ItemToPurchase();
       ItemToPurchase item2 = new ItemToPurchase();
 
       // Get item 1 details from user, create itemToPurchase object
+      System.out.println("Item 1");
+      System.out.println("Enter the item name: ");
+      productName = scnr.nextLine();
+      System.out.println("Enter the item price: ");
+      productPrice = scnr.nextInt();
+      System.out.println("Enter the item quantity: ");
+      productQuantity = scnr.nextInt();
+      item1.setName(productName);
+      item1.setPrice(productPrice);
+      item1.setQuantity(productQuantity);
       
+      //Tests
+      System.out.println(item1.getName());
+      System.out.println(item1.getPrice());
+      System.out.println(item1.getQuantity());
  
       // Get item 2 details from user, create itemToPurchase object
-
+      System.out.println("Item 2");
+      System.out.println("Enter the item name: ");
+      productName = scnr.nextLine();
+      System.out.println("Enter the item price: ");
+      productPrice = scnr.nextInt();
+      System.out.println("Enter the item quantity: ");
+      productQuantity = scnr.nextInt();
+      item2.setName(productName);
+      item2.setPrice(productPrice);
+      item2.setQuantity(productQuantity);
+      
+      //Tests
+      System.out.println(item2.getName());
+      System.out.println(item2.getPrice());
+      System.out.println(item2.getQuantity());
       
       // Add costs of two items and print total
       // cartTotal = item one price + item two price
