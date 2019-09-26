b2f7251b08a981e42f6d00fe15315457e7a76b26
Jeff <jmperkinson@gmail.com>
2019-09-26 15:24:44 -0400

    Add getters & setters, Monkey, Grazioso Salvare.

--- eclipse-workspace/GraziosoSalvare/src/Monkey.java
+++ eclipse-workspace/GraziosoSalvare/src/Monkey.java
@@ -44,7 +44,92 @@ public class Monkey extends RescueAnimal {
     
     // Accessor methods
     
+    /* Getter methods establish an interface to read class fields.
+     * Each method has a return type matching the type of the field
+     * the method is accessing.
+     */
+    
+    public String getSpecies() {
+        return species;
+    }
+    
+    public float getTailLength() {
+        return tailLength;
+    }
+    
+    public float getHeight() {
+        return height;
+    }
+    
+    public float getBodyLength() {
+        return bodyLength;
+    }
+    
+    public float getTorsoSize() {
+        return torsoSize;
+    }
+    
+    public float getSkullSize() {
+        return skullSize;
+    }
+    
+    public float getNeckSize() {
+        return neckSize;
+    }
     
     // Mutator methods
+    
+    /* Setter methods establish an interface to write class fields.
+     * Methods have a void return type.  A return statement is
+     * still provided even with the void return type in order to
+     * clear the stack frame (i.e. local variable space created
+     * for method execution)
+     * Method parameters are of type matching the type of the
+     * field being modified through the method.
+     * In many cases for naming simplicity, method parameters share 
+     * the same identifier as the field being modified.  
+     * Making use of the 'this' keyword clarifies ambiguity.  
+     * The object's field value is set to the argument 
+     * passed in to the method call.  However, the team has chosen
+     * a different approach, as evidenced in the Dog class.
+     * For consistency, the parameter identifier in the below
+     * setter methods differs from the member field identifier
+     * and the 'this' keyword is not utilized.
+     */
+    
+    public void setSpecies(String animalSpecies) {
+        species = animalSpecies;
+        return;
+    }
+    
+    public void setTailLength(float animalTailLength) {
+        tailLength = animalTailLength;
+        return;
+    }
+    
+    public void setHeight(float animalHeight) {
+        height = animalHeight;
+        return;
+    }
+    
+    public void setBodyLength(float animalBodyLength) {
+        bodyLength = animalBodyLength;
+        return;
+    }
+    
+    public void setTorsoSize(float animalTorsoSize) {
+        torsoSize = animalTorsoSize;
+        return;
+    }
+    
+    public void setSkullSize(float animalSkullSize) {
+        skullSize = animalSkullSize;
+        return;
+    }
+    
+    public void setNeckSize(float animalNeckSize) {
+        neckSize = animalNeckSize;
+        return;
+    }
     
 }
