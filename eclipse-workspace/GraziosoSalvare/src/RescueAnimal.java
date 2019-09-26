b1cd94917f287386126360e561a6f740d8ee9aa0
Jeff <jmperkinson@gmail.com>
2019-09-25 21:26:57 -0400

    Add getters for GraziosoSalvare RescueAnimal class.

--- eclipse-workspace/GraziosoSalvare/src/RescueAnimal.java
+++ eclipse-workspace/GraziosoSalvare/src/RescueAnimal.java
@@ -1,10 +1,22 @@
 
-
+/* The SimpleDateFormat class is used to format dates according to
+ * the format specified by the class user at object instantiation.
+ * This class is imported here to facilitate date formatting
+ * for the class fields representing date values.
+ * http://tutorials.jenkov.com/java-internationalization/simpledateformat.html
+ */
 import java.text.SimpleDateFormat;
 
 public class RescueAnimal {
 
     // Class variables
+    /* Access modifiers are private to promote encapsulation.
+     * Encapsulation restricts direct access to member fields,
+     * only exposing read write access through public methods
+     * (getters and setters defined below).  This practice 
+     * controls how outside classes can influence the values
+     * of these member fields.  
+     */
     private String name;
     private String type;
     private String gender;
@@ -28,14 +40,95 @@ public class RescueAnimal {
     private String inServicePhone;
     private String inServicePostalAddress;
 
-    // Constructor
+    // Default constructor
     public RescueAnimal() {
+    
     }
 
-    // Add Accessor Methods here
-
-
-    // Add Mutator Methods here
+    // Accessor Methods (getters)
+    // Access modifiers are public to promote encapsulation practices
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public float getWeight() {
+        return weight;
+    }
+    
+    public SimpleDateFormat getAcquisitionDate() {
+        return acquisitionDate;
+    }
+    
+    public SimpleDateFormat getStatusDate() {
+        return statusDate;
+    }
+    
+    public String getAcquisitionSource() {
+        return acquisitionSource;
+    }
+    
+    public boolean getReserved() {
+        return reserved;
+    }
+    
+    public String getTrainingLocation() {
+        return trainingLocation;
+    }
+    
+    public SimpleDateFormat getTrainingStart() {
+        return trainingStart;
+    }
+    
+    public SimpleDateFormat getTrainingEnd() {
+        return trainingEnd;
+    }
+    
+    public String getTrainingStatus() {
+        return trainingStatus;
+    }
+    
+    public String getInServiceCountry() {
+        return inServiceCountry;
+    }
+    
+    public String getInServiceCity() {
+        return inServiceCity;
+    }
+    
+    public String getInServiceAgency() {
+        return inServiceAgency;
+    }
+    
+    public String getInServicePOC() {
+        return inServicePOC;
+    }
+    
+    public String getInServiceEmail() {
+        return inServiceEmail;
+    }
+    
+    public String getInServicePhone() {
+        return inServicePhone;
+    }
+    
+    public String getInServicePostalAddress() {
+        return inServicePostalAddress;
+    }
+    
+    // Mutator Methods (setters)
 
 
 }
