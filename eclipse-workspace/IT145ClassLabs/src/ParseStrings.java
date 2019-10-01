6a602083a79a97d6168e1b6582086d8596232a23
Jeff <jmperkinson@gmail.com>
2019-09-30 20:53:11 -0400

    Ch6 lab ParseStrings initial commit

---
+++ eclipse-workspace/IT145ClassLabs/src/ParseStrings.java
@@ -0,0 +1,11 @@
+
+public class ParseStrings {
+
+    public static void main(String[] args) {
+        
+        
+        
+        return; //return even when return type is void
+                //to clear the stack frame
+    }
+}
