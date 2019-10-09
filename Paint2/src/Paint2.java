fb55a279fa79d77512c9fc6749f2bd5591e18d52
Jeff <jmperkinson@gmail.com>
2019-10-09 06:40:48 -0400

    Add Paint2.java exception handling.

--- Paint2/src/Paint2.java
+++ Paint2/src/Paint2.java
@@ -6,6 +6,10 @@
 
 import java.util.Scanner;
 
+//Import Java's Double class for input parsing and double type conversion
+//to aide in input validation
+import java.lang.Double;
+
 //import the Math class from java.lang package
 //to use Math.ceil() for determining the number of cans
 //needed.
@@ -17,6 +21,11 @@ public class Paint2 {
     public static void main(String[] args) {
 
         Scanner scnr = new Scanner(System.in);
+        //String variables needed to store user input for parsing
+        //and conversion to type double for input validation
+        String wallHeightAsString;
+        String wallWidthAsString;
+
         double wallHeight = 0.0;
         double wallWidth = 0.0;
         double wallArea = 0.0;
@@ -26,14 +35,67 @@ public class Paint2 {
 
         final double squareFeetPerGallons = 350.0;
         final double gallonsPerCan = 1.0;
+        boolean validHeight = true;     //boolean value to control do-while
+                                        //loop for user's wall height input
+
+        boolean validWidth = true;      //boolean value to control do-while
+                                        //loop for user's wall width input
 
         // Prompt user to input wall's height
-        System.out.println("Enter wall height (feet): ");
-        wallHeight = scnr.nextDouble();
+        // Implement a do-while loop with try-catch to ensure input is valid and any exceptions are handled
+
+        do {
+            validHeight = true;
+            try {
+                System.out.println("Enter wall height (feet): ");
+                //Store the input in a String for parsing and double type conversion
+                wallHeightAsString = scnr.nextLine();
+                //Convert user input to type double
+                //If conversion does not occur an exception will
+                //be thrown that is handled by the catch block
+                wallHeight = Double.parseDouble(wallHeightAsString);
+                
+                if (wallHeight <= 0) {
+                    throw new Exception("Invalid height.");
+                }
+            }
+            catch (NumberFormatException excpt) {
+                System.out.println("Do not enter String. " + excpt.getMessage());
+                validHeight = false;
+            }
+            catch (Exception excpt) {
+                System.out.println("Bad value exception. " + excpt.getMessage());
+                validHeight = false;
+            }
+        } while (!validHeight);
 
         // Prompt user to input wall's width
-        System.out.println("Enter wall width (feet): ");
-        wallWidth = scnr.nextDouble();
+        // Implement a do-while loop with try-catch to ensure input is valid and any exceptions are handled
+
+        do {
+            validWidth = true;
+            try {
+                System.out.println("Enter wall width (feet): ");
+                //Store the input in a String for parsing and double type conversion
+                wallWidthAsString = scnr.nextLine();
+                //Convert user input to type double
+                //If conversion does not occur an exception will
+                //be thrown that is handled by the catch block
+                wallWidth = Double.parseDouble(wallWidthAsString);
+                
+                if (wallWidth <= 0) {
+                    throw new Exception("Invalid width.");
+                }
+            }
+            catch (NumberFormatException excpt) {
+                System.out.println("Do not enter String. " + excpt.getMessage());
+                validWidth= false;
+            }
+            catch (Exception excpt) {
+                System.out.println("Bad value exception. " + excpt.getMessage());
+                validWidth = false;
+            }
+        } while (!validWidth);
 
         // Calculate and output wall area
         wallArea = wallHeight * wallWidth;
@@ -57,4 +119,3 @@ public class Paint2 {
          System.out.println("Cans needed: " + cansNeeded + " can(s)");
     }
 }
-
